723c6c4ea678624750dc6b94a58dd2326a7be940
Shashank Aditya <msrsaditya@gmail.com>
2023-10-13 16:06:41 +0530

    Create cutebot.py

---
+++ cutebot.py
@@ -0,0 +1,20 @@
+import openai
+import os
+import colorama
+
+openai.api_key = "<Enter Your API Key Here>"
+model_engine = "gpt-3.5-turbo"
+
+while True:
+    print("")
+    prompt = input(colorama.Fore.YELLOW + "Ask Me Anything: " + colorama.Style.RESET_ALL)
+    if prompt.lower() == "exit":
+        break
+    completion = openai.ChatCompletion.create(
+        model=model_engine,
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+    response = completion.choices[0].message['content']
+    print(colorama.Fore.GREEN + response + colorama.Style.RESET_ALL, end='\n\n')
